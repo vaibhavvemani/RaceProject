22d05f0fc0677a1b2815b3ded6117c5a4af4f13e
vaibhavvemani <59450124+vaibhavvemani@users.noreply.github.com>
2023-05-18 11:41:02 +0530

    Frist Commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+
+</script>
+
+<main>
+  <header>
+    <h1>Let's race</h1>
+    <nav>
+      <ul class="navitems">
+        <li>
+          Teams
+        </li>
+        <li>
+          Drivers
+        </li>
+        <li>
+          <button class="contactbtn">Contact</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+
+  <div class="landingpagecontainer">
+    <div class="leftside">
+      <h1><q>If everything seems <span class="color-effect">under</span> control, you're just not going <span class="color-effect">fast</span> enough.</q></h1>
+      <div class="salebtncontainer">
+        <button class="salebtn raceme" >Out of control</button>
+        <button class="salebtn wanker" >In control</button>
+      </div>
+    </div>
+    <div class="rightside">
+      <h4>Defining speed:</h4>
+      <div class="framecontainer">
+
+        <div class="bubble"></div>
+        <div class="bubble2"></div>
+        <div class="bubble3"></div>
+
+        <div class="frame"></div>
+
+        <div class="rightbubble"></div>
+        <div class="rightbubble2"></div>
+      </div>
+    </div>
+  </div>
+</main>
